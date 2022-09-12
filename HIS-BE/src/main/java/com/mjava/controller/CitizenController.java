71281dbf494a7154d99568450acaa4eeb1887027
Mani <Mani@LAPTOP-DV1J9JTR>
2022-09-12 13:58:13 +0530

    t++

--- HIS-BE/src/main/java/com/mjava/controller/CitizenController.java
+++ HIS-BE/src/main/java/com/mjava/controller/CitizenController.java
@@ -19,7 +19,7 @@ public class CitizenController {
 	@PostMapping("/insertcitizen")
 	public  CitizenEntity  insertCitizen(@RequestBody CitizenEntity citizen)
 	{
-		//testing gi
+		//testing git
 	 return citizenService.insertCitizenService(citizen);
 		 
 	}
